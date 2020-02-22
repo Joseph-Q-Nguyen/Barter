70f3a03cbad3de08a9c0048603955efcdd02af28
Dev Kapupara <devkapupara@gmail.com>
2020-02-22 02:30:26 -0800

    Added connection to PostgresSQL deployed on AWS. Updated requirements.txt, so update your forks. You will need to add the file '_set_envs.pth' to venv/lib/python3.7/site_packages/ which is included in this repository

--- Barter/Barter/settings.py
+++ Barter/Barter/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'Barter.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'barter',
+    'USER': os.environ['BARTER_USER'],
+    'PASSWORD': os.environ['BARTER_PWD'],
+    'HOST': 'barter.crfnzn2kfilh.us-west-1.rds.amazonaws.com',
+    'PORT': '5432',
     }
+
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
