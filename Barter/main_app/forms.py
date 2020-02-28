a07e681e326fc1289065f765e70589d19d5b9637
Dev Kapupara <devkapupara@gmail.com>
2020-02-27 16:46:13 -0800

    Configured user authentication, updated reset_db.bash script, added RegistrationFrom and QOL fixes

--- Barter/main_app/forms.py
+++ Barter/main_app/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 
+text_widget = forms.TextInput(attrs={'class':'form-control'})
+pwd_widget = forms.PasswordInput(attrs={'class':'form-control'})
+
 class LoginForm(forms.Form):
-	username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class':'form-control'}))
-	password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+	username = forms.CharField(required=False, label='Username', widget=text_widget)
+	password = forms.CharField(required=False, label='Password', widget=pwd_widget)
 
 	# def clean(self):
 	# 	data = super().clean()
@@ -10,3 +13,19 @@ class LoginForm(forms.Form):
 	# 		raise forms.ValidationError("Username field cannot be empty")
 	# 	if len(data['password']) == 0:
 	# 		raise forms.ValidationError("Password field cannot be empty")
+
+class RegisterForm(forms.Form):
+	name = forms.CharField(label='Full Name', widget=text_widget)
+	email = forms.EmailField(label='Email', widget=text_widget)
+	phone = forms.CharField(label='Phone Number', widget=text_widget)
+	password = forms.CharField(label='Password', widget=pwd_widget)
+	confirm_password = forms.CharField(label='Confirm Password', widget=pwd_widget)
+
+	def clean(self):
+		data = super().clean()
+		email = data['email']
+		print(email)
+		if email.split('@'[1]) != 'sjsu.edu':
+			raise ValidationError("Email domain must belong to San Jose State University")
+		if password != confirm_password:
+			raise ValidationError("Passwords do not match.")
--- Barter/main_app/views.py
+++ Barter/main_app/views.py
@@ -1,25 +1,54 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from django.urls import reverse
+from django.forms import ValidationError
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm, RegisterForm
 from .models import Item
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-	return render(request, "index.html", context={'posts': Item.objects.all()})
+	context = {'posts': Item.objects.all()}
+	if not request.user.is_anonymous:
+		context['logged_in'] = True
+		print(request.user)
+		context['user'] = request.user
+		print(request.user.first_name)
+	return render(request, "index.html", context=context)
 
-def login(request):
-	form = LoginForm()
-	context = {'logged_in': False, 'login_form': form}
+def login_user(request):
+	login_form = LoginForm()
+	context = {'is_login': True, 'logged_in': False, 'form': login_form}
 	if request.method == "POST":
-		lform = LoginForm(request.POST)
-		if lform.is_valid():
-			print(lform)
-			username = lform['username']
-			password = lform['password']
-			if 'login' in lform:
-				print("Logging in..")
+		data = request.POST
+		print(data)
+		login_form = LoginForm(data)
+		if login_form.is_valid():
+			if 'register' in data:
+				register_form = RegisterForm()
+				context['is_login'] = False
+				context['form'] = register_form
+				return render(request, 'main_app/signin.html', context=context)
+			elif 'sign_up' in data:
+				name = data['name']
+				uname, lname = name.split(' ')
+				email = data['email']
+				pwd = data['password']
+				user = User.objects.get_or_create(username=''.join(name.lower().split(' ')), password=pwd, email=email, first_name=uname, last_name=lname)
+				return render(request, 'main_app/signin.html', context=context)
 			else:
-				print("Registering...")
-			user = authenticate(request, username=username, password=password)
-			context['logged_in'] = user != None
+				username = data['username']
+				password = data['password']
+				print(username, password)
+				user = authenticate(request, username=username, password=password)
+				if user is None:
+					raise ValidationError("Incorrect Username/Password")
+				login(request, user=user)
+				context['logged_in'] = user != None
+				context['user'] = user.first_name
+				return render(request, 'index.html', context=context)
 	return render(request, "main_app/signin.html", context=context)
+
+def logout_user(request):
+	logout(request)
+	return render(request, 'index.html', context={'logged_in': False})
