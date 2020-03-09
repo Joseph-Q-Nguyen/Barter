21e847c8c2764c498c5398995308fa7180de2737
Joseph Nguyen <joseph.q.nguyen@sjsu.edu>
2020-03-08 23:53:45 -0700

    Added previous and next buttons that allows users to go to the previous or next page with at most 10 posts

--- Barter/main_app/views.py
+++ Barter/main_app/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from .models import Item
 
 # Create your views here.
-def index(request, offset, limit):
-	return render(request, "index.html", context={'posts': Item.objects.all()[offset:limit]})
+def index(request):
+	if (request.method == "GET"):
+		offset = request.GET.get("offset")
+		if offset and offset.isdigit():
+			offset = int(offset)
+		else:
+			offset = 0
+		return render(request, "index.html", context={'posts': Item.objects.all()[offset: offset + 10]})
+	else:
+		offset_string = request.GET.get("offset")
+		offset = 0
+		if offset_string and offset_string.isdigit():
+			offset = int(offset_string)
+			if "next" in request.POST:
+				offset = offset + 10
+			else:
+				offset = offset - 10
+		return redirect("http://localhost:8000/?offset=" + str(offset))
 
 def login(request):
 	form = LoginForm()
