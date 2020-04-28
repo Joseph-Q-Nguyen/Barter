ea08705a572d6b6db10cb8a615b14209a6c4e009
Dev Kapupara <devkapupara@gmail.com>
2020-04-27 23:12:09 -0700

    New User Register Check implemented

--- Barter/main_app/forms.py
+++ Barter/main_app/forms.py
@@ -7,28 +7,30 @@ class LoginForm(forms.Form):
 	username = forms.CharField(required=False, label='Username', widget=text_widget)
 	password = forms.CharField(required=False, label='Password', widget=pwd_widget)
 
-	# def clean(self):
-	# 	data = super().clean()
-	# 	if len(data['username']) == 0:
-	# 		raise forms.ValidationError("Username field cannot be empty")
-	# 	if len(data['password']) == 0:
-	# 		raise forms.ValidationError("Password field cannot be empty")
+	def clean(self):
+		data = super().clean()
+		if len(data['username']) == 0:
+			raise forms.ValidationError("Username field cannot be empty")
+		if len(data['password']) == 0:
+			raise forms.ValidationError("Password field cannot be empty")
 
 class RegisterForm(forms.Form):
 	name = forms.CharField(label='Full Name', widget=text_widget)
 	email = forms.EmailField(label='Email', widget=text_widget)
-	phone = forms.CharField(label='Phone Number', widget=text_widget)
 	password = forms.CharField(label='Password', widget=pwd_widget)
 	confirm_password = forms.CharField(label='Confirm Password', widget=pwd_widget)
 
 	def clean(self):
 		data = super().clean()
 		email = data['email']
+		pwd = data['password']
+		c_pwd = data['confirm_password']
 		print(email)
 		if email.split('@')[1] != 'sjsu.edu':
-			raise ValidationError("Email domain must belong to San Jose State University")
-		if password != confirm_password:
-			raise ValidationError("Passwords do not match.")
+			raise forms.ValidationError("Email domain must belong to San Jose State University")
+		if pwd != c_pwd:
+			raise forms.ValidationError("Passwords do not match.")
+		return self.data
 
 class ListingForm(forms.Form):
 	FN = 'FN'
