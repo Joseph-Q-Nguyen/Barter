144eb9edad55c9e3fe0bb415cb2bc504eca4ea20
Joseph Nguyen <joseph.q.nguyen@sjsu.edu>
2020-04-28 21:36:07 -0700

    unit tests

--- Barter/main_app/tests.py
+++ Barter/main_app/tests.py
@@ -1,14 +1,17 @@
 from django.test import TestCase
 from django.test import Client
+from django.contrib.auth.models import User
+from .models import Item
+import uuid
 
 # Create your tests here.
 
 OK = 200
 FOUND = 302
 
-VALID_USERNAME = 'cheryltownsend'
-VALID_PASSWORD = 'pwdcheryltownsend1234'
-VALID_ITEM_PID = 'pwdcheryltownsend1234'
+VALID_USERNAME = 'testuser'
+VALID_PASSWORD = '123123'
+VALID_ITEM_PID = uuid.uuid1()
 
 class UnitTester(TestCase):
     @classmethod
@@ -17,11 +20,15 @@ class UnitTester(TestCase):
 
     def setUp(self):
         self.client = Client()
+        user = User.objects.get_or_create(username=VALID_USERNAME, email='test@sjsu.edu', first_name='test', last_name='user')[0]
+        user.set_password(VALID_PASSWORD)
+        user.save()
+        Item.objects.get_or_create(user=user, pid=VALID_ITEM_PID, category='ST', title="test item", date_posted="2020-01-01", description="test description", price="100")
 
     def test_update_post(self):
         response = self.client.post('/login', {'username': VALID_USERNAME, 'password': VALID_PASSWORD})
-        response2 = self.client.get('update_listing/%3FPd037d5fc-89b4-11ea-960e-3af9d372bd52%5Cw+')
-        response3 = self.client.post('update_listing/%3FPd037d5fc-89b4-11ea-960e-3af9d372bd52%5Cw+', 
+        response2 = self.client.get(f'update_listing/{VALID_ITEM_PID}')
+        response3 = self.client.post(f'update_listing/{VALID_ITEM_PID}', 
         {
             'title': 123,
             'category': 'ST',
@@ -31,9 +38,10 @@ class UnitTester(TestCase):
         self.assertEqual(response.status_code, OK)
 
     def test_delete_post(self):
-        response = self.client.post('/login', {'username': 'cheryltownsend', 'password': 'pwdcheryltownsend1234'})
-        response2 = self.client.get('delete_listing/%3FPd037d5fc-89b4-11ea-960e-3af9d372bd52%5Cw+')
+        response = self.client.post('/login', {'username': VALID_USERNAME, 'password': VALID_PASSWORD})
+        response2 = self.client.get(f'delete_listing/{VALID_ITEM_PID}')
         self.assertEqual(response.status_code, OK)
+
 
     
 
