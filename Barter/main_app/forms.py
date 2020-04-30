55afd3b5275dd40fc40d560ac244259bc4ab4c33
William Luna <42703238+William-Luna@users.noreply.github.com>
2020-04-30 05:06:27 -0700

    added listing creation tests, fixed bugs

--- Barter/main_app/forms.py
+++ Barter/main_app/forms.py
@@ -63,5 +63,5 @@ class ListingForm(forms.Form):
 
 	title= forms.CharField(label='Item Name', widget=text_widget)
 	category=forms.CharField(label='Category', widget=forms.Select(attrs={'class':'custom-select'}, choices=categories))
-	price=forms.DecimalField(label='Price', widget=forms.NumberInput(attrs={'class':"form-control"}))
+	price=forms.DecimalField(label='Price', widget=forms.NumberInput(attrs={'class':"form-control"}), min_value=0.01)
 	description=forms.CharField(label='Description', widget=forms.Textarea(attrs={'class':'form-control'}))
